f4cc8dd6f708ba234319d218adc9bc9d83613149
jini <dlwlsgml1106@gmail.com>
2025-07-14 10:49:57 +0900

    test

---
+++ js/url.js
@@ -0,0 +1,19 @@
+function home() {
+    location.href = "http://127.0.0.1:5501/sub/home.html"
+}
+
+function about() {
+    location.href = "http://127.0.0.1:5501/sub/about.html"
+}
+
+function artist() {
+    location.href = "http://127.0.0.1:5501/sub/artist.html"
+}
+
+function schedule() {
+    location.href = "http://127.0.0.1:5501/sub/schedule.html"
+}
+
+function contact() {
+    location.href = "http://127.0.0.1:5501/sub/contact.html"
+}
